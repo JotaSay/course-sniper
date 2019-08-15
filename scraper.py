d727533a80356cfdc51cdc77c9e43a6657d79046
Jeancarlo Bolanos <jeancarlobolanos@JCs-MacBook-Pro.local>
2019-08-15 13:06:11 -0400

    Updates to scraper

--- scraper.py
+++ scraper.py
@@ -14,15 +14,16 @@ def sniped():
         subCode = request.form['subCode']
         courCode = request.form['courCode']
         secCode = request.form['secCode']
-        url = "https://sis.rutgers.edu/soc/courses.json?subject={}&semester=92016&campus=NB&level=U,G".format(subCode)
+        url = "https://sis.rutgers.edu/soc/courses.json?subject={}&semester=92019&campus=NB&level=U,G".format(subCode)
         r = requests.get(url)
         data = r.json()
         for c in data:
             if c['courseNumber']==courCode:
                 for d in c['sections']:
-                    if d['number']==secCode and d['openStatus']=="true":
+                    print(d['number'], d['openStatus'])
+                    if d['number']==secCode and d['openStatus']==True:
                         return render_template('sniperload.html',opened = "true")
-                    else:
-                        return render_template('sniperload.html',opened = "false")
+
+        return render_template('sniperload.html',opened = "false")
     else:
         return render_template('index.html')
